86720c252b552b0fa8a1f77b4a077283554c9112
Winston Venderbush <winstonvenderbush@gmail.com>
2017-04-21 14:39:34 -0400

    finished in class ;)

--- comp.py
+++ comp.py
@@ -2,8 +2,24 @@
 #Constantine Athanitis
 
 def union(list1, list2):
-	list3 = [ x for x in list1 and y for y in list2 if y not in list2]
+	list3 = list1 + [x for x in list2 if x not in list1]
 	return list3
 
+def intersect(list1, list2):
+	list3 = [x for x in list1 if x in list2]
+	return list3
 
-print union([1, 2, 3], [2, 3, 4])
+def difference(list1, list2):
+	list3 = [x for x in list1 if x not in list2]
+	return list3
+
+def symdiff(list1, list2):
+	list3 = [x for x in list1 if x not in list2] + [x for x in list2 if x not in list1]
+	return list3
+
+def cartprod(list1, list2):
+	list3 = [(x, y) for x in list1 for y in list2]
+	return list3
+
+#print cartprod([1, 2, 3], [2, 3, 4])
+#print cartprod([2, 3, 4], [1, 2, 3])
